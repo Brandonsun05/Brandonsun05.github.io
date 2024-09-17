af59936d6d2eebd08d7e0075b458945bb156268b
Brandonsun05 <brandonsun419@gmail.com>
2024-09-17 16:23:17 +1000

    week 7

---
+++ week7/media player/script.js
@@ -0,0 +1,42 @@
+const notify = document.querySelector("#notify");
+console.log(notify);
+
+const playButton = document.querySelector("#play-button");
+console.log(playButton);
+
+playButton.addEventListener("click", playAudio);
+function playAudio() {
+  notify.play();
+}
+
+const pauseButton = document.querySelector("#pause-button");
+console.log(pauseButton);
+
+pauseButton.addEventListener("click", pauseAudio);
+function pauseAudio() {
+  notify.pause();
+}
+const popSound = document.querySelector("#pop-sound");
+console.log(popSound);
+
+const popButton = document.querySelector("#pop-button");
+console.log(popButton);
+
+popButton.addEventListener("click", popAudio);
+function popAudio() {
+  popSound.play();
+}
+
+const playButton = document.querySelector("#play-button");
+console.log(playButton);
+playButton.addEventListener("click", playAudio);
+function playAudio() {
+  myVideo.play();
+}
+
+const pauseButton = document.querySelector("#pause-button");
+console.log(pauseButton);
+pauseButton.addEventListener("click", pauseAudio);
+function pauseAudio() {
+  myVideo.pause();
+}
---
+++ week7/script.js
@@ -0,0 +1,68 @@
+// const myButton = document.querySelector("#my-button");
+// console.log(myButton);
+
+// myButton.addEventListener("click", myFunction);
+// let clickCount = 0;
+
+// function myFunction() {
+//   console.log("did you just click me?");
+//   clickCount++;
+//   count.textContent=clickCount
+// }
+
+// const count = document.querySelector("#count");
+
+const boxContainer = document.querySelector(".box-container");
+console.log(boxContainer);
+
+const toggleButton = document.querySelector("#toggle-button");
+console.log(toggleButton);
+toggleButton.addEventListener("click", toggleMe);
+
+function toggleMe() {
+  console.log("toggle button is clicked");
+  boxContainer.classList.toggle("row-reverse");
+}
+
+const addButton = document.querySelector("#add-button");
+console.log(addButton);
+addButton.addEventListener("click", addMe);
+
+const removeButton = document.querySelector("#remove-button");
+console.log(removeButton);
+removeButton.addEventListener("click", removeMe);
+
+// function AddMe() {
+//   console.log("add button is clicked");
+//   boxContainer.innerHTML += `<div class="box purple-box"></div>
+//         <div class="box coral-box"></div>`;
+// }
+
+let count = 0;
+function addMe() {
+  console.log("add buttn is clicked");
+  if (count % 2 === 0) {
+    boxContainer.innerHTML += `<div class="box purple-box"></div>`;
+  } else {
+    boxContainer.innerHTML += `<div class="box coral-box"></div>`;
+  }
+  count++;
+}
+
+function removeMe() {
+  let lastBox = boxContainer.lastElementChild;
+  console.log(lastBox);
+  if (lastBox) {
+    lastBox.remove();
+  }
+  count--;
+}
+
+boxContainer.addEventListener("mouseenter", dropMe);
+boxContainer.addEventListener("mouseleave", pickMe);
+
+function dropMe() {
+  boxContainer.classList.add("drop");
+}
+
+function pickMe() {}
